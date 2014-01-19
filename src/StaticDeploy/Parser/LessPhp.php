54e9d3f6c19c3644764e6c8eac6c07c3fe1a8534
Timon <dev@timonf.de>
2014-01-19 19:40:39 +0100

    added less parser

---
+++ src/StaticDeploy/Parser/LessPhp.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of StaticDeploy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StaticDeploy\Parser;
+
+use lessc as LessCompiler;
+
+/**
+ * Less parser. Will parse less(php) files.
+ *
+ * @author Timon <dev@timonf.de>
+ */
+class LessPhp implements ParserInterface
+{
+    /**
+     * @inherit()
+     */
+    public function parse($resource)
+    {
+        $lessPhp = new LessCompiler();
+        $content = $lessPhp->compileFile($resource);
+
+        return $content;
+    }
+
+    /**
+     * @inherit()
+     */
+    public function getCapableExtension()
+    {
+        return array(
+            'less'
+        );
+    }
+
+    /**
+     * @inherit()
+     */
+    public function getOutputFilename($filename)
+    {
+        // we want to convert 'example.less' to 'example.css' and
+        // 'example.css.less' to 'example.css'
+        if (strpos('.css', $filename) > 0) {
+            return str_replace('.less', '', $filename);
+        } else {
+            return str_replace('.less', '.css', $filename);
+        }
+    }
+
+}
